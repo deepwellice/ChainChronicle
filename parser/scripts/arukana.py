0d2641b1ce876959faf546db247845f1ab860127
deepwellice <deepwellice@users.noreply.github.com>
2016-08-28 11:05:22 +0800

    comment out some prints to make output clean. may add log function later if needed.

--- parser/scripts/arukana.py
+++ parser/scripts/arukana.py
@@ -18,6 +18,8 @@ class ArukanaDownloader:
         self.json = None
         self.log_lock = threading.Lock()
         self.thread_list = []
+        self.force_list = [1, 3, 14, 1003, 3002, 4001, 4003, 4016, 4017,
+                           4022, 5003, 6008, 7501, 7508]
 
     def download_all(self, cid_range):
         for cid in cid_range:
@@ -25,20 +27,25 @@ class ArukanaDownloader:
                                                args=(range(cid, cid+1)))
             download_thread.start()
             self.thread_list.append(download_thread)
-            if len(self.thread_list)==10:
+            if len(self.thread_list)==20:
                 for td in self.thread_list:
                     if td.is_alive():
                         td.join()
                 self.thread_list.clear()
 
     def download(self, cid):
+        force_download = False
+        if cid in self.force_list:
+            force_download = True
+
         local_path = os.path.join(self.download_folder,
                                   '{0:05d}.scr'.format(cid))
-        if os.path.exists(local_path):
-            print("download skipped: {0}".format(cid))
-            return 304
+        if not force_download:
+            if os.path.exists(local_path):
+                # print("download skipped: {0}".format(cid))
+                return 304
 
-        print("download started: {0}".format(cid))
+        #print("download started: {0}".format(cid))
         download_url = '{0}cha_2d_card_{1:05d}.scr'.format(self.url, cid)
         retry_count = 10
         while retry_count > 0:
@@ -47,14 +54,14 @@ class ArukanaDownloader:
                 download_response = requests.get(download_url, stream=True,
                                                  timeout=180)
                 ret_code = download_response.status_code
-                return
                 if ret_code != 200:
-                    print("downaload failure: {0}".format(ret_code))
+                    #print("downaload failed: {0}:{1}".format(cid, ret_code))
                     return ret_code
                 with open(local_path, 'wb') as local_fd:
                     for chunk in download_response.iter_content(1024):
                         local_fd.write(chunk)
-                self.log_history(cid)
+                if not force_download:
+                    self.log_history(cid)
                 print('Download successfully: {0}'.format(cid))
                 return ret_code
             except requests.RequestException:
@@ -87,5 +94,6 @@ class ArukanaDownloader:
 if __name__ == "__main__":
     test_downloader = ArukanaDownloader()
     test_downloader.download_all(range(1,70001))
+    # test_downloader.download(1063)
     # for cid in range(1, 70001):
     #    test_downloader.download(cid)
